693edf3114baa1394e03b2e347430e7140869ce1
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-09-07 16:00:28 +0200

    feat: el juego ya tiene un recorrido circular

--- quest/game.py
+++ quest/game.py
@@ -10,12 +10,16 @@ class Quest:
         self.display = pg.display.set_mode((ANCHO_P, ALTO_P))
         pg.display.set_caption("THE QUEST")
         pg.mixer.init()
-        self.pantallas = [
-            Pantalla(self.display),
-            PantallaPrincipal(self.display),
-            PantallaJuego(self.display),PantallaJuego2(self.display),
-            PantallaRecords(self.display)]
+        self.pantallas = [PantallaPrincipal(self.display),
+                          PantallaJuego(self.display), PantallaJuego2(
+                              self.display),
+                          PantallaRecords(self.display)]
 
     def jugar(self):
-        for pantalla in self.pantallas:
-            pantalla.bucle_principal()
+        contador_pantallas = 0
+        while contador_pantallas < 4:
+            pantalla_activa = self.pantallas[contador_pantallas]
+            pantalla_activa.bucle_principal()
+            contador_pantallas += 1
+            if contador_pantallas == 4:
+                contador_pantallas = 0
